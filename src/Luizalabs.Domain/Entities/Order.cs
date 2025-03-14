7cc710f0eaa6ea3bf2a2934d7bffd05209a93b65
caue <pablorborges@gmail.com>
2025-03-14 10:00:05 -0300

    feature: Criação dos entities, interfaces e dtos

---
+++ src/Luizalabs.Domain/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luizalabs.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; init; }
+        public decimal Total { get; set; }
+        public DateTime Date { get; init; }
+        public List<Product> Products { get; } = new();
+        public int UserId { get; init; }
+    }
+}
---
+++ src/Luizalabs.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using Luizalabs.Application.Dtos;
+using Luizalabs.Domain.Entities;
+
+namespace Luizalabs.Domain.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task BulkInsertAsync(IEnumerable<User> users);
+        Task<IEnumerable<OrderResponse>> GetOrdersAsync(int? orderId, DateTime? startDate, DateTime? endDate);
+    }
+}
