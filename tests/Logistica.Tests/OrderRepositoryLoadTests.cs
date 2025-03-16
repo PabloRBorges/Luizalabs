84649bc4473bbd83a4fd0d58d61c11b9eb67aa7f
caue <pablorborges@gmail.com>
2025-03-16 19:53:01 -0300

    feat: inclusão de teste de carga

---
+++ tests/Logistica.Tests/OrderRepositoryLoadTests.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LogisticaVertical.Domain.Dtos;
+using LogisticaVertical.Domain.Interfaces;
+using Moq;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using Oracle.ManagedDataAccess.Types;
+using Xunit;
+
+public class OrderRepositoryLoadTests
+{
+    private readonly Mock<IOrderRepository> _orderRepository;
+
+    public OrderRepositoryLoadTests()
+    {
+        _orderRepository = new Mock<IOrderRepository>();
+    }
+
+    [Fact(DisplayName = "Load test 10 seconds")]
+    [Trait("Categoria", "GetOrder")]
+    public void LoadTest_GetOrdersAsync()
+    {
+        // arr
+        _orderRepository.Setup(repo => repo.GetOrdersAsync(It.IsAny<int?>(), It.IsAny<DateTime?>(), It.IsAny<DateTime?>()))
+            .ReturnsAsync(new List<OrderResponse>()); // Simulando resposta vazia
+
+        //act
+        var scenario = Scenario.Create("GetOrders_LoadTest", async context =>
+        {
+            await _orderRepository.Object.GetOrdersAsync(null, null, null);
+            return Response.Ok();
+        })
+            .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+            .WithLoadSimulations(
+                // 10 requisições por segundo durante 10s
+                Simulation.Inject(rate: 10, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(10)) 
+            );
+
+        //assert
+        var result = NBomberRunner.RegisterScenarios(scenario).Run();
+        Assert.True(result.AllOkCount > 0);
+    }
+}
